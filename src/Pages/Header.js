6ac8a385e583b13a02324e3cf8226b2d82ba8e7a
unknown <nagi5924@gmail.com>
2023-09-26 21:36:04 +0900

    [feat] header 작업중

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -2,9 +2,25 @@ const Header = () => {
   return (
     <>
       <header className='header__container'>
-        <div className='list__container'></div>
-        <div className='logo__container'></div>
-        <div className='btn__container'></div>
+        <div className='upperContainer'>
+          <div className='list__container'>
+            <div>HOME</div>
+            <div>SHOP</div>
+            <div>ABOUT</div>
+          </div>
+          <div className='btn__container'>
+            <div>Q</div>
+            <div>SINGN IN/LOGIN</div>
+            <div>CART</div>
+          </div>
+        </div>
+        <div className='underContainer'>
+          <div className='logo__container'>
+            <p>River</p>
+            <p>'s</p>
+            <p>Staionary Shop</p>
+          </div>
+        </div>
       </header>
     </>
   );
